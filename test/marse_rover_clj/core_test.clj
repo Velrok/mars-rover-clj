93fa113d03f19ca1e1f6543f57890702b25de560
Waldemar <waldemar.schwan@uswitch.com>
2019-09-17 23:25:55 +0100

    sanity check robot turning

--- test/marse_rover_clj/core_test.clj
+++ test/marse_rover_clj/core_test.clj
@@ -1,4 +1,16 @@
 (ns marse-rover-clj.core-test
-  (:require [clojure.test :refer :all]
-            [marse-rover-clj.core :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [marse-rover-clj.core :refer [process-instruction]]))
+
+(deftest robot-turning-left-cicle
+  (is (= [{:orientation \N} {:orientation \W} {:orientation \S} {:orientation \E} {:orientation \N}]
+         (reductions process-instruction
+                     {:orientation \N}
+                     (seq "LLLL")))))
+
+(deftest robot-turning-right-cicle
+  (is (= [{:orientation \N} {:orientation \E} {:orientation \S} {:orientation \W} {:orientation \N}]
+         (reductions process-instruction
+                     {:orientation \N}
+                     (seq "RRRR")))))
 
