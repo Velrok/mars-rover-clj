b57349c6557a4041665fdf8f13770228fe2609f4
Waldemar <waldemar.schwan@uswitch.com>
2019-09-17 22:29:22 +0100

    Initial commit

---
+++ test/marse_rover_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns marse-rover-clj.core-test
+  (:require [clojure.test :refer :all]
+            [marse-rover-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
